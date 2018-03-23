da6f45bf198414860014dfe07e4566a517bacdb5
ResNet <resnet@rachelcarson-169-233-75-184.resnet.ucsc.edu>
2018-03-23 14:02:09 -0700

    cmdln arg opens file and gets first line in nonuseful manner

--- prac.cpp
+++ prac.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 // get arguments
@@ -7,12 +9,20 @@ using namespace std;
     // SZ followed by title\n   
 
 int main(int argc, char** argv){
-    cout << " You Have Entered " << argc
-         << " arguments:" << "\n";
-
+    // Body to catch which file is being parsed 
+    cout << "File selected " << argv[1] << endl;
+    
     for (int i=0; i<argc; ++i){
+        if( argv[i] == "\n") { cout << "nl found at " << argv[i] << endl; }
         cout << argv[i] << "\n";
     }
-
+    
+    // Body to open file and read from it
+    string line;
+    ifstream inFile ( argv[1] );
+    if( inFile ){ // same as: (inFile.good()) basically not EOF
+        getline( inFile, line );
+        cout << line << endl;
+    }
     return 0;
 }
