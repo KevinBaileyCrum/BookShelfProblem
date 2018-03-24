72e41c3c820b4c81e7b84ffe51160ce253d043ba
Kevin Crum <kevinbaileycrum@gmail.com>
2018-03-24 01:51:37 -0700

    tokenized input into a vector<int>

--- prac.cpp
+++ prac.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <vector>
 using namespace std;
 
 // get arguments
@@ -9,7 +11,7 @@ using namespace std;
     // SZ followed by title\n   
 
 int main(int argc, char** argv){
-    // Body to catch which file is being parsed 
+
     cout << "File selected " << argv[1] << endl;
     
     for (int i=0; i<argc; ++i){
@@ -20,9 +22,22 @@ int main(int argc, char** argv){
     string line;
     ifstream inFile ( argv[1] );
     if( inFile ){ // same as: (inFile.good()) basically not EOF
+        
         getline( inFile, line );
         cout << line << endl;
-        cout << "try line[0] " << line[0] << line[1] << endl;
+        
+        //cout << "try line[0] " << line[0] << line[1] << endl;
+        istringstream iss(line); // string steam iss takes in current line
+        vector<int> shelves;    
+        int s;
+        while( iss>>s ){        // while we can push new ints from iss ont s
+            shelves.push_back(s);
+        }
+        for( int i=0; i<shelves.size(); ++i ){
+            cout << "shelves["<<i<<"] " << shelves[i] << ' ';
+        }
+        cout << "\n";
+    
     }
     return 0;
 }
